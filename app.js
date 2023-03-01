2482b9b0358611853cf026eaba8385ed0883c1be
Anuja Bujurge <62695141+anujabujurge@users.noreply.github.com>
2023-03-01 15:12:10 -0500

    first

---
+++ app.js
@@ -0,0 +1,44 @@
+//Exercise 1
+
+// let marks = 65;
+// if (marks >= 90) {
+//   console.log("A");
+// } else if (marks >= 80) {
+//   console.log("B");
+// } else if (marks >= 70) {
+//   console.log("C");
+// } else if(marks >=55){
+//   console.log("D");
+// }else{
+//     console.log("F");
+// }
+
+// // Exercise 2
+// for(let i=10; i<=6; i+=2){
+//     console.log("value = " +i);
+// }
+
+// for(let i=10; i>=0; i--){
+//     console.log("Value " +i);
+// }clearInterval
+// console.log("odd numbers");
+// for(let i=1; i<=10; i+=2){
+//     console.log(i);
+// }
+// console.log("Even numbers");
+// for(let i=2; i<=10; i+=2){
+    
+//     console.log(i);
+// }
+
+// for(let i=2; i<=20; i++){
+//     console.log(i*3);
+// }
+
+// let tri="#"
+// for(i=0;i<=7;i++){
+    
+//     console.log(tri);
+//     tri+= '#'
+// }
+
